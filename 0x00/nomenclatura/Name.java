63fb0c209125df169b85446a657adb37c1ccc4b8
Bruno Afanasiev <bruno.afanasiev@gmail.com>
2025-10-06 14:30:31 -0300

    0. Uso da Nomenclatura

---
+++ 0x00/nomenclatura/Name.java
@@ -0,0 +1,30 @@
+import java.net.URL;
+import java.util.Date;
+import java.util.List;
+
+public class Name {
+
+    public Date modifiedDate;
+
+    public List<Testcase> find(Name.User user) {};
+
+    public List<Account> accountList;
+
+    public void deleteUser(Name.User user) { }
+
+    public URL url;
+
+    public class TableRepresenter {}
+
+    public void saveCar() {}
+
+    public void saveUser() {  }
+
+    public void createAccount() {  }
+
+    public void generateContract() {    }
+
+    public static final int DAYS_IN_YEAR = 360;
+    public static final int HOURS_IN_WEEK = 168;
+
+}
